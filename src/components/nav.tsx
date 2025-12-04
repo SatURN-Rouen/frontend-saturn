6b229ab12fcddeb9ed1d1d61d5c0b34150691f20
Mirabelle SALLES <theo.salles@univ-rouen.fr>
2025-12-04 23:56:39 +0100

    Nav #1

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,8 +2,22 @@ import "./styles/nav.css";
 
 function Nav() {
   return (
-    <div className="nav">
-    </div>
+    <nav className="nav">
+        <div id={"left"}>
+            <a href={"/"}>
+                <img alt={"Logo"} src={"https://nird.forge.apps.education.fr/img/logo+text206px.png"} />
+            </a>
+            <div id={"linksBar"}>
+                <a href={"/scenarios"}>Scenarios</a>
+                <a href={"https://nird.forge.apps.education.fr/"}>Notre site</a>
+                <a href={"/scenarios"}>Nous contacter</a>
+                <a href={"/scenarios"}>Connexion</a>
+            </div>
+        </div>
+        <div id={"right"}>
+
+        </div>
+    </nav>
   );
 }
 
