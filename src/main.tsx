c5b5fa09efe1d22166c48fdafa3f2b5479c625cd
Mirabelle SALLES <theo.salles@univ-rouen.fr>
2025-12-05 00:31:21 +0100

    Routes #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Home from './routes/Home.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<Home />} />
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import Nav from '../components/nav.tsx';
+
+import './styles/App.css'
+
+function Home() {
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default Home
