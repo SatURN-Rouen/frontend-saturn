2122b45bd48e24c0324a1878e406ff6d5931fd54
Mirabelle SALLES <theo.salles@univ-rouen.fr>
2025-12-05 02:00:44 +0100

    Misc components

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import Home from './routes/Home.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
-import Nav from "./components/nav.tsx";
+import Nav from "./components/Nav.tsx";
+import Scenarios from "./routes/Scenarios.tsx";
 import Chat from "./routes/Chat.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Nav/>
         <BrowserRouter>
+            <Nav/>
             <Routes>
                 <Route path={"/"} element={<Home/>}/>
                 <Route path={"/chat"} element={<Chat/>}/>
+                <Route path={"/:profile"} element={<Scenarios/>}/>
             </Routes>
         </BrowserRouter>
     </StrictMode>,
---
+++ src/routes/Scenarios.tsx
@@ -0,0 +1,23 @@
+import './styles/Scenarios.css'
+import {useParams} from "react-router";
+import ScenarioCard from "../components/ScenarioCard.tsx";
+
+
+function Scenarios() {
+
+    const $params = useParams();
+    console.log($params.profile)
+
+    return (
+        <div id={"Scenarios"}>
+            <h1>J'utilise...</h1>
+            <div className={"scenarios"}>
+                <ScenarioCard img={"https://images.unsplash.com/photo-1684127987312-43455fd95925?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGxhZ2d5JTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D"} title={"“Mon PC n’est pas compatible avec Windows 11 !”"} url={"/scenarios/xxx"} />
+                <ScenarioCard img={"https://images.unsplash.com/photo-1684127987312-43455fd95925?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGxhZ2d5JTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D"} title={"“Mon PC n’est pas compatible avec Windows 11 !”"} url={"/scenarios/xxx"} />
+                <ScenarioCard img={"https://images.unsplash.com/photo-1684127987312-43455fd95925?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGxhZ2d5JTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D"} title={"“Mon PC n’est pas compatible avec Windows 11 !”"} url={"/scenarios/xxx"} />
+            </div>
+        </div>
+    )
+}
+
+export default Scenarios
